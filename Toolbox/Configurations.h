46217df1601173dfcf1d756dc10e436bf8b45206
Prasanna-source31 <139847095+Prasanna-source31@users.noreply.github.com>
2023-08-08 12:32:34 +0530

    Add files via upload

---
+++ Toolbox/Configurations.h
@@ -0,0 +1,75 @@
+#ifndef CONF_H  
+#define CONF_H
+
+#include <iostream>
+using namespace std;
+class Configurations {
+public:
+ static string connectionCheckerURL;
+ static string fingerprinterURL;
+ static string fingerprinter;
+ static string logFolderName;
+ static string option;
+ static string optionde;
+ static string defaulten;
+ static string defaultde;
+ static string advanced;
+ static string  advancedde;
+static string  german;
+static string  english;
+static string  connection;
+static string  connectionde;
+static string  folder;
+static string  folderde;
+static string  download;
+static string  downloadde;
+static string  ACL;
+static string  ACLde;
+static string  schedule;
+static string  weekday1;
+static string  weekday2;
+static string  weekday3;
+static string  weekday4;
+static string  weekday5;
+static string  weekdayde1;
+static string  weekdayde2;
+static string  weekdayde3;
+static string  weekdayde4;
+static string  weekdayde5;
+static string  randomize;
+static string  randomizeadv;
+static string  randomizeadvde;
+static string  randomizede;
+static string  textbox11;
+static string  onstart;
+static string  onstartde;
+static string  start;
+static string  textbox8;
+static string textbox15;
+static string taskfailed;
+static string connectionfailed;
+static string skippedfailed;
+static string connectionsuccess;
+static string tasksuccess;
+static string folderexists;
+static string foldersuccess;
+static string folderfailure;
+static string folderwarning;
+static string taskfailedde;
+static string connectionfailedde;
+static string skippedfailedde;
+static string connectionsuccessde;
+static string folderexistsde;
+static string foldersuccessde;
+static string folderfailurede;
+static string tasksuccessde;
+static string showlog;
+static string folderwarningde;
+static string dailyde;
+static string weeklyde;
+ static string monthlyde;
+ 
+ 
+ };
+
+#endif
