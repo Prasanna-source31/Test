919769f15d9f5d9a9b0f5cd83af8352246978619
Prasanna-source31 <139847095+Prasanna-source31@users.noreply.github.com>
2023-08-09 10:40:16 +0530

    Add files via upload

---
+++ Toolbox/fingerprinter.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <intrin.h>
+
+using namespace std;
+
+int main()
+{
+    // Get extended ids.
+    int CPUInfo[4] = {-1};
+    __cpuid(CPUInfo, 0x80000000);
+    unsigned int nExIds = CPUInfo[0];
+
+    // Get the information associated with each extended ID.
+    char CPUBrandString[0x40] = { 0 };
+    for( unsigned int i=0x80000000; i<=nExIds; ++i)
+    {
+        __cpuid(CPUInfo, i);
+
+        // Interpret CPU brand string and cache information.
+        if  (i == 0x80000002)
+        {
+            memcpy( CPUBrandString,
+            CPUInfo,
+            sizeof(CPUInfo));
+        }
+        else if( i == 0x80000003 )
+        {
+            memcpy( CPUBrandString + 16,
+            CPUInfo,
+            sizeof(CPUInfo));
+        }
+        else if( i == 0x80000004 )
+        {
+            memcpy(CPUBrandString + 32, CPUInfo, sizeof(CPUInfo));
+        }
+}
+
+    cout  << CPUBrandString;
+}
