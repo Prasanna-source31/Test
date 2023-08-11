26b180d2faa4a9571e0d2044bde4d33c488ffe81
Prasanna-source31 <139847095+Prasanna-source31@users.noreply.github.com>
2023-08-11 15:22:57 +0530

    Add files via upload

--- mai1.tf
+++ mai1.tf
@@ -1,20 +1,17 @@
-resource "aws_security_group" "sg_ping" {
-   name = "Allow Ping"
-
--  ingress {
--    from_port       = -1
--    to_port         = -1
--    protocol        = "icmp"
--    security_groups = [aws_security_group.sg_8080.id]
--  }
- }
-
- resource "aws_security_group" "sg_8080" {
-  name = "Allow 8080"
-  egress {
-    // ...
-    cidr_blocks = "invalid_cidr"  // Incorrect format
-  }
+provider "aws" {
+  region = "us-west-2"
 }
-   }
- }
+
+resource "aws_instance" "example" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+
+  # Deliberate mistake: Missing "tags" block
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket"
+  acl    = "public-read"
+  
+  # Deliberate mistake: Missing "tags" block
+}
